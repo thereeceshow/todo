41b313b4cf7e8fe03474260386a5b511cfbe7db6
Reece Walter <reece@reecewalter.com>
2021-03-24 19:38:33 -0400

    still need two more onclick functinos for delete all, uncomplete all

--- src/Task.js
+++ src/Task.js
@@ -1,15 +1,16 @@
 import React from "react";
 
+
 function Task(props) {
   return (
-    <li className="list-group-item d-flex">
+    <li className={props.done ? "text-decoration-line-through text-muted list-group-item d-flex" : "list-group-item d-flex"}>
       <div>
         <div
           className=" me-3 border-2 border-secondary d-flex justify-content-start"
-          onClick={() => props.completeThis(props.keyID)}
+          onClick={() => props.completeThis(props.taskNumber, 'done')}
         >
           {props.done ? (
-            <i className="bi bi-check-circle"></i>
+            <i className="bi bi-check-circle text-success"></i>
           ) : (
             <i className="bi bi-circle"></i>
           )}
@@ -18,7 +19,7 @@ function Task(props) {
       {props.text}
       <i
         className="bi bi-x fs-3 ms-3 px-3 d-flex justify-content-end position-absolute end-0"
-        onClick={() => props.closeThisB(props.keyID)}
+        onClick={() => props.completeThis(props.taskNumber, 'deleted')}
       />
     </li>
   );
